d4d0a079bad623bc272800f80a9374026b0be313
ooflet <ooflet@proton.me>
2026-09-01 19:30:50 +1000

    im going to lose it

--- components/ScrollCards.tsx
+++ components/ScrollCards.tsx
@@ -61,7 +61,7 @@ export function ScrollCards() {
             ref={(el) => {
               cardRefs.current[index] = el;
             }}
-            className="relative min-h-[100dvh] bg-[var(--background)] px-4 py-6 md:px-6"
+            className="relative min-h-[100svh] bg-[var(--background)] px-4 py-6 md:px-6"
             style={{ zIndex: index + 1 }}
           >
             {card.content}
